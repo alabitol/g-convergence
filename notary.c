b2aa145b3c460f6a5e6b14b95884fcc4ff9b907d
Martin Dluhos <martin@gnu.org>
2012-03-06 13:37:03 -0600

    Added argument processing to notary.c

--- notary.c
+++ notary.c
@@ -1,10 +1,10 @@
-/******************************************************************************
+/*****************************************************************************
  * Authors: Tolu Alabi
  *          Zach Butler
  *          Martin Dluhos
  *
  * Created: February 27, 2012
- * Revised: March 4, 2012
+ * Revised: March 5, 2012
  * Description: This program starts the notary server which listens for
  * verification requests from clients.
  ******************************************************************************/
@@ -14,6 +14,9 @@
 #include "connection.h"
 #include "certificate.h"
 #include "response.h"
+
+/* Keep track of the number of clients with active requests. */
+unsigned int num_active_clients = 0;
 
 /* This is our primary datasructure for storing requests that arrive from
  * clients. Each request will contain a URL of the host to be verified. If the
@@ -35,22 +38,106 @@ initiateLogging ()
   return 1;
 }
 
+/* Print a helpful usage message if the user calls notary incorrectly or
+ * if she invokes it with --help flag. */
+void 
+print_usage ()
+{
+
+} // print_help
+
 int main (int argc, char *argv[])
 {
+  int i;
+
   struct MHD_Daemon *daemon;
+  unsigned short port;
 
   /* Process command line arguments. */
+  for (i = 1; i < argc; i++)
+  {
+    if ((! strcmp (argv[i], "-help")) || (! strcmp (argv[i], "--help")))
+    {
+      print_usage ();
+    }
+    else if (! strcmp (argv[i], "-p"))
+    {
+      http_port = atoi (argv[++i]);
+    }
+    else if (! strcmp (argv[i], "-s"))
+    {
+      ssl_port = atoi (argv[++i]);
+    }
+    else if (! strcmp (argv[i], "-i"))
+    {
+      ip = argv[++i];
+    }
+    else if (! strcmp (argv[i], "-c"))
+    {
+      certificate_file = argv[++i];
+    }
+    else if (! strcmp (argv[i], "-k"))
+    {
+      key_file = argv[++i];
+    }
+    else if (! strcmp (argv[i], "-u"))
+    {
+      username = argv[++i];
+    }
+    else if (! strcmp (argv[i], "-g"))
+    {
+      group = argv[++i];
+    }
+    else if (! strcmp (argv[i], "-d"))
+    {
+      debug = 1;
+    }
+    else if (! strcmp (argv[i], "-f"))
+    {
+      foreground = 1;
+    }
+    else if (! strcmp (argv[i], "-b"))
+    {
+      backend = argv[++i];
+    }
+    else if (! strcmp (argv[i], "-h"))
+    {
+     print_usage ();
+    }
+  }
 
-  /* Print a helpful usage message if the user calls notary incorrectly or
-   * if she invokes it with --help flag. */
 
   initiateLogging ();
 
-  /* Start the MHD daemon to listen for client requests. We probably want to
-   * use one thread per connection passing MHD_USE_THREAD_PER_CONNECTION as
-   * the first argument.
+  /* Make sure we can start the daemon in the background. */
+
+  /* Start the MHD daemon to listen for client requests. 
+   * Parameters: 
+   * MHD_USE_THREAD_PER_CONNECTION: use one thread per connection 
+   * port: port to listen on
+   * NULL: allow connection from any IP
+   * NULL: additional arguments to preceding param
+   * &answer_to_connection: call this function to handle a new connection
+   * NULL: arguments to answer_to_connection,
+   * MHD_OPTION_END: indicate that there are no more options
    */
-  daemon = MHD_start_daemon (...);
+  daemon = MHD_start_daemon (MHD_USE_THREAD_PER_CONNECTION, 
+                             port, 
+			     NULL, 
+			     NULL, 
+			     &answer_to_connection, NULL, MHD_OPTION_NOTIFY_COMPLETED, 
+			     request_completed, NULL, MHD_OPTION_END);
+
+  if (daemon == NULL)
+  {
+    fprintf (stderr, "Error: Failed to start the MHD daemon\n")
+    return 1;
+  }
+  else
+    printf ("MHD daemon is listening on port %d\n", port);
+
+  /* Prevent the server from stopping immediately after starting. */
+  get_char ();
 
   /* Stop the  MHD daemon. */
   MHD_stop_daemon (daemon);
