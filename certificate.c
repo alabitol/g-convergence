090175d8338bd2264b62c9440b3f89c5a7913233
Zachary Butler <butlerza@grinnell.edu>
2012-03-11 17:08:36 -0500

    Changed implementation of verify_certificate to make it more elegant.

--- certificate.c
+++ certificate.c
@@ -29,8 +29,5 @@ request_certificate (char *url, char *fingerprint_from_website)
 int
 verify_certificate (char *fingerprint_from_client, char *fingerprint_from_website)
 {
-  if (strcmp (fingerprint_from_client, fingerprint_from_website) == 0)
-    return 1;
-  else
-    return 0;
+  return !strcmp(finger_from_client, fingerprint_from_website);
 }
