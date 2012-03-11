bda0a212ca9923c1b848882f603e9055560c7dee
Tolu Alabi <alabitol@grinnell.edu>
2012-03-11 18:17:48 -0500

    Radhika: Got rid of errors in connection.c and wrote request_completed()

--- response.h
+++ response.h
@@ -13,13 +13,13 @@
 
 /* Obtains a response to a POST/GET request. */
 int 
-retrieve_response (void *coninfo_cls, char* url, char *fingerprint_from_client);
+retrieve_response (void *coninfo_cls, const char* url, const char *fingerprint_from_client);
 
 /* Sends response back to the client. This function could be a wrapper for
  * send_page.
  */
 int
-send_response (struct MHD_Connection *connection, const char *response_data
+send_response (struct MHD_Connection *connection, const char *response_data,
                int status_code);
 
-#ifndef // RESPONSE_H
+#endif // RESPONSE_H
