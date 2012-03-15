65598269293be20d7d758675c46ce428d3bdbdd3
Zachary Butler <butlerza@grinnell.edu>
2012-03-15 16:37:39 -0500

    Added a version of request_certificate that returns the string form of the certificate. I still have yet to figure out how to turn certificates into DER binary in C so that we can do the SHA-1 fingerprint.

--- certificate.c
+++ certificate.c
@@ -14,17 +14,47 @@
  * fingerprint of the certificate into fingerprint_from_website. Return 1 if
  * certificate was obtained. Otherwise, return 0.
  */
-int 
-request_certificate (char *url, char *fingerprint_from_website)
+char *
+request_certificate (char *url)
 {
-  /* We need to figure out how to send an HTTP request to the
-   * website. There is no microhttpd function for this.
-   */
-  CURL *handle;
-
-  handle = curl_easy_init();
-  curl_easy_setopt(handle, CURLOPT_URL, url);
-  curl_easy_setopt(handle, CURL_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+  
+  CURL *curl;
+  CURLcode res;
+ 
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+ 
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, wrfu);
+ 
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+ 
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+    curl_easy_setopt(curl, CURLOPT_CERTINFO, 1L);
+ 
+    res = curl_easy_perform(curl);
+ 
+    if(!res) {
+      struct curl_certinfo *ci = NULL;
+ 
+      res = curl_easy_getinfo(curl, CURLINFO_CERTINFO, &ci);
+ 
+      if(!res && ci) {
+        struct curl_slist *slist;
+        for(slist = ci->certinfo[0]; slist; slist = slist->next)
+          if(!strncmp(slist->data, "Cert:", 5))
+            printf("%s\n", slist->data+5);
+      }
+    }
+  
+ 
+    curl_easy_cleanup(curl);
+  }
+ 
+  curl_global_cleanup();
 }
 
 /* Verifies that the fingerprint from the website matches the
