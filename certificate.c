15727cc4a6ceb7052c9294433736228e9caa37de
Zachary Butler <butlerza@grinnell.edu>
2012-03-15 16:06:27 -0500

    Made a couple of changes that will probably be overwritten in the next commit.

--- certificate.c
+++ certificate.c
@@ -8,6 +8,7 @@
  * Description: Handle sending a certificate request to the website and
  *              receiving its response.
  ******************************************************************************/
+#include<curl/curl.h>
 
 /* Requests a certificate from the website given by the url. Saved the
  * fingerprint of the certificate into fingerprint_from_website. Return 1 if
@@ -19,7 +20,11 @@ request_certificate (char *url, char *fingerprint_from_website)
   /* We need to figure out how to send an HTTP request to the
    * website. There is no microhttpd function for this.
    */
-  return 1;
+  CURL *handle;
+
+  handle = curl_easy_init();
+  curl_easy_setopt(handle, CURLOPT_URL, url);
+  curl_easy_setopt(handle, CURL_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 }
 
 /* Verifies that the fingerprint from the website matches the
